135d3e4b33cfc30d308db302bc4ebae8f5cbf60e
Giuseppe Ambesi <giuseppeambesi@Giuseppes-MacBook-Air.local>
2013-04-08 17:03:46 +0200

    Initial Commit

---
+++ tutorial/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  tutorial
+//
+//  Created by Giuseppe Ambesi on 08.04.13.
+//  Copyright (c) 2013 Giuseppe Ambesi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
