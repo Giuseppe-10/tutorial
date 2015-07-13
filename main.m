649d3d583eacfe3e58e222f434b8d8588c5fcb8d
Giuseppe-10 <giuseppe.ambesi@hotmail.it>
2015-07-13 16:09:39 +0200

    Tried some things with cmd

---
+++ main.m
@@ -0,0 +1,29 @@
+//
+//  main.m
+//  tutorial
+//
+//  Created by Giuseppe Ambesi on 08.04.13.
+//  Copyright (c) 2013 Giuseppe Ambesi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // hello guys i'm giuseppe...
+        
+        int n1 = 20;
+        int n2 = 40;
+        int sum = n1 + n2;
+         
+        NSLog(@"the sum of %i and %i is %i", n1, n2, sum);
+        
+        
+        
+    }
+    return 0;
+}
+
